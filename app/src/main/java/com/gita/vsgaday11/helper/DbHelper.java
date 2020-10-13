3dfd3aa3b312933972159fd0dd030aedaa27b1a2
Gita Nuraini <42348805+GitaNuraini@users.noreply.github.com>
2020-10-13 16:04:42 +0700

    add

--- app/src/main/java/com/gita/vsgaday11/helper/DbHelper.java
+++ app/src/main/java/com/gita/vsgaday11/helper/DbHelper.java
@@ -1,8 +1,13 @@
 package com.gita.vsgaday11.helper;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DbHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 2;
@@ -28,5 +33,53 @@ public class DbHelper extends SQLiteOpenHelper {
                 ")";
 
         db.execSQL(SQL_CREATE_MOVIE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS" + TABLE_SQLite);
+        onCreate(db);
+    }
+    public ArrayList<HashMap<String, String>> getAllData() {
+        ArrayList<HashMap<String, String>> wordList;
+        wordList = new ArrayList<HashMap<String, String>>();
+        String selectQuery = "SELECT * FROM" + TABLE_SQLite;
+        SQLiteDatabase database = this.getWritableDatabase();
+        Cursor cursor = database.rawQuery(selectQuery, null);
+        if(cursor.moveToFirst()) {
+            do{
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put(COLUMN_ID, cursor.getString(0));
+                map.put(COLUMN_NAME, cursor.getString(1));
+                map.put(COLUMN_ADDRESS, cursor.getString(2));
+                wordList.add(map);
+            } while (cursor.moveToNext());
+        }
+        Log.e("select sqlite", "" + wordList);
+
+        database.close();
+        return wordList;
+    }
+    public void insert (String name, String address) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        String queryValues = "INSERT INTO" + TABLE_SQLite + "(name, address)" +
+                "VALUES ('" + name + "', '" +address +"')";
+
+        Log.e("insert sqlite", "" + queryValues);
+        database.execSQL(queryValues);
+        database.close();
+    }
+
+    public void update(int id, String name, String address){
+        SQLiteDatabase database = this.getWritableDatabase();
+
+        String updateQuery = "UPDATE" + TABLE_SQLite + "SET"
+                + COLUMN_NAME + "='" + name + "', "
+                + COLUMN_ADDRESS + "='" + address + "' "
+                + "WHERE" + COLUMN_ID + "=" + "'" + id + "'";
+        Log.e("update sqlite", updateQuery);
+        database.execSQL(updateQuery);
+        database.close();
+
     }
 }
